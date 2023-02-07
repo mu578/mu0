6f777e1b66d3cf4da1d8cf6f17f099262525c243
mu578 <19998532+mu578@users.noreply.github.com>
2023-02-07 10:44:14 -0800

    indirection on _Generic keyword

--- mu0/mu0_definition/mu0_feature.h
+++ mu0/mu0_definition/mu0_feature.h
@@ -50,10 +50,25 @@
 #	define MU0_HAVE_EXTENSION 0
 if (((defined(__GNUC__) && __GNUC__ >= 4) || (defined(__clang__) || defined(__llvm__))))
 #		undef  MU0_HAVE_EXTENSION
-#		define MU0_HAVE_EXTENSION 0
+#		define MU0_HAVE_EXTENSION 1
 #		define __mu0_extension__ __extension__
 #	else
 #		define __mu0_extension__
+#	endif
+
+#	undef  MU0_HAVE_GENERIC
+#	undef  __mu0_generic__
+#	define MU0_HAVE_EXTENSION 0
+if defined(__STDC__) || defined(__STDC_VERSION__)
+#		if (__STDC__ == 1 && __STDC_VERSION__ >= 201112L)
+#			undef  MU0_HAVE_GENERIC
+#			define MU0_HAVE_GENERIC 1
+#			define __mu0_generic__ _Generic
+#		else
+#			define __mu0_generic__
+#		endif
+#	else
+#		define __mu0_generic__
 #	endif
 
 #endif /* !MU0_FEATURE_H */
--- mu0/mu0_definition/mu0_language.h
+++ mu0/mu0_definition/mu0_language.h
@@ -97,6 +97,7 @@
 #				define MU0_HAVE_C23 1
 #			endif
 #		endif
+#		endif
 #	endif
 
 #	if !MU0_HAVE_C11 && !MU0_HAVE_CPP11
