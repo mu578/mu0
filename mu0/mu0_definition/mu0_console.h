d359cbaad597177bdbc3ed2e619597e1fbd05393
mu578 <19998532+mu578@users.noreply.github.com>
2023-02-18 16:36:45 -0800

    adding bitset and console indirection

--- mu0/mu0_definition/mu0_console.h
+++ mu0/mu0_definition/mu0_console.h
@@ -34,16 +34,16 @@
 #	endif
 
 #	define __mu0_bitset_print_u8__(__x) \
-	__mu0_console__("" __mu0_bitset_pattern_u8__ "\n", __mu0_bitset_format_u8__(AFG_CAST(uint8_t, (__x))))
+	__mu0_console__("" __mu0_bitset_pattern_u8__ "\n", __mu0_bitset_format_u8__(((unsigned char)(__x))))
 
 #	define __mu0_bitset_print_u16__(__x) \
-	__mu0_console__("" __mu0_bitset_pattern_u16__ "\n", __mu0_bitset_format_u16__(AFG_CAST(uint16_t, (__x))))
+	__mu0_console__("" __mu0_bitset_pattern_u16__ "\n", __mu0_bitset_format_u16__(((unsigned short)(__x))))
 
 #	define __mu0_bitset_print_u32__(__x) \
-	__mu0_console__("" __mu0_bitset_pattern_u32__ "\n", __mu0_bitset_format_u32__(AFG_CAST(uint32_t, (__x))))
+	__mu0_console__("" __mu0_bitset_pattern_u32__ "\n", __mu0_bitset_format_u32__(((unsigned int)(__x))))
 
 #	define __mu0_bitset_print_u64__(__x) \
-	__mu0_console__("" __mu0_bitset_pattern_u64__ "\n", __mu0_bitset_format_u64__(AFG_CAST(uint64_t, (__x))))
+	__mu0_console__("" __mu0_bitset_pattern_u64__ "\n", __mu0_bitset_format_u64__(((unsigned long long)(__x))))
 
 #endif /* !MU0_CONSOLE_H */
 
