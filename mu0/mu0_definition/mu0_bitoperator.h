677e7c703685b00913625998e8611b9e7b244ce3
mu578 <19998532+mu578@users.noreply.github.com>
2023-04-30 09:35:17 -0700

    eliminating part of char arith promotion issue

--- mu0/mu0_definition/mu0_bitoperator.h
+++ mu0/mu0_definition/mu0_bitoperator.h
@@ -944,14 +944,14 @@ const ___mu0_uint1_t___ __mu0_bit_floor_u1__(const ___mu0_uint1_t___ __x)
 #			define __mu0_bit_cast__(__a, __b) \
 			(__a) = __builtin_bit_cast(__mu0_typeof__((__mu0_typeof__(__x))(__x)), __b)
 #		else
-#			define __mu0_bit_cast__(__a, __b)                                                          \
-			__mu0_memset__(__mu0_addressof__(__b), 0                     , __mu0_sizeof__((__b) + 0)); \
-			__mu0_memcpy__(__mu0_addressof__(__b), __mu0_addressof__(__a), __mu0_sizeof__((__b) + 0))
+#			define __mu0_bit_cast__(__a, __b)                                                      \
+			__mu0_memset__(__mu0_addressof__(__b), 0                     , __mu0_sizeof__((__b))); \
+			__mu0_memcpy__(__mu0_addressof__(__b), __mu0_addressof__(__a), __mu0_sizeof__((__b)))
 #		endif
 #	else
-#	define __mu0_bit_cast__(__a, __b)                                                          \
-	__mu0_memset__(__mu0_addressof__(__b), 0                     , __mu0_sizeof__((__b) + 0)); \
-	__mu0_memcpy__(__mu0_addressof__(__b), __mu0_addressof__(__a), __mu0_sizeof__((__b) + 0))
+#	define __mu0_bit_cast__(__a, __b)                                                      \
+	__mu0_memset__(__mu0_addressof__(__b), 0                     , __mu0_sizeof__((__b))); \
+	__mu0_memcpy__(__mu0_addressof__(__b), __mu0_addressof__(__a), __mu0_sizeof__((__b)))
 #	endif
 
 #	if !MU0_HAVE_BITOPERATOR
