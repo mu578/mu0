a61433db89b4254b8700d042bf58b7b6badc13f6
mu578 <19998532+mu578@users.noreply.github.com>
2023-05-20 14:22:15 -0700

    doing the right thing

--- mk/mu0.mk
+++ mk/mu0.mk
@@ -25,11 +25,10 @@ include $(MU0_ROOT_PATH)/mk/inc/mu0_source.mk
 include $(MU0_ROOT_PATH)/mk/inc/mu0_toolchain.mk
 include $(MU0_ROOT_PATH)/mk/inc/mu0_ruleset.mk
 
-all       :    rule_all
+all       : rule_all
 static    : rule_static
 shared    : rule_shared
-clean     :  rule_clean
-
-distcheck : rule_all
+clean     : rule_clean
+distcheck : rule_all rule_static rule_clean
 
 # EOF
