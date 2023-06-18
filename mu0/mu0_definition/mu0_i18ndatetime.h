3b98ea1e992ea0e1af957a261f0da31b44ae6acc
mu578 <19998532+mu578@users.noreply.github.com>
2023-06-18 16:30:38 -0700

    cleanups

--- mu0/mu0_definition/mu0_i18ndatetime.h
+++ mu0/mu0_definition/mu0_i18ndatetime.h
@@ -16,7 +16,7 @@
 //
 
 #include <mu0/mu0_definition/mu0_clocktime.h>
-#include <mu0/mu0_definition/mu0_locale.h>
+#include <mu0/mu0_definition/mu0_i18nlocale.h>
 
 #ifndef MU0_I18NDATETIME_H
 #define MU0_I18NDATETIME_H 1
@@ -36,21 +36,21 @@ typedef struct tm __mu0_calendar_date_t__;
 #	define __mu0_layout_rfc3339_full_utc__  "%Y-%m-%d %H:%M:%OSZ"
 #	define __mu0_layout_rfc3339_utc__       "%Y-%m-%d %H:%M:%SZ"
 
-#	define __mu0_format_iso8601_full__     0
-#	define __mu0_format_iso8601__          1
-#	define __mu0_format_iso8601_full_utc__ 2
-#	define __mu0_format_iso8601_utc__      3
+#	define __mu0_format_iso8601_full__      0
+#	define __mu0_format_iso8601__           1
+#	define __mu0_format_iso8601_full_utc__  2
+#	define __mu0_format_iso8601_utc__       3
 
-#	define __mu0_format_rfc3339_full__     4
-#	define __mu0_format_rfc3339__          5
-#	define __mu0_format_rfc3339_full_utc__ 6
-#	define __mu0_format_rfc3339_utc__      7
+#	define __mu0_format_rfc3339_full__      4
+#	define __mu0_format_rfc3339__           5
+#	define __mu0_format_rfc3339_full_utc__  6
+#	define __mu0_format_rfc3339_utc__       7
 
 __mu0_static_inline__
 const ___mu0_sint4_t___ __mu0_i18ndatetime_formatting__(
 	  const __mu0_calendar_date_t__ * __date
 	, const ___mu0_sint4_t___         __format
-	, const ___mu0_i18nlocale_t___    __locale __mu0_nullable__
+	, const __mu0_i18nlocale_t__      __locale __mu0_nullable__
 	, ___mu0_tint1_t___               __dest[64]
 ) {
 	return 0;
